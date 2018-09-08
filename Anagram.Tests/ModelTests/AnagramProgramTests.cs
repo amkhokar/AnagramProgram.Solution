56f78decb77d3d3af3e3c7219a7e2bafd395075c
Ahmed Khokar <amkhokar@gmail.com>
2018-09-08 03:28:21 -0700

    add test structure

---
+++ Anagram.Tests/ModelTests/AnagramProgramTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anagram; 
+
+namespace Anagram.Tests
+{
+    [TestClass]
+    public class AnagramProgramTests
+    {
+        [TestMethod]
+        public void isBoolean()
+        {
+
+        }
+    }
+}
---
+++ Anagram/Models/AnagramProgram.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Anagram 
+{
+    public class AnagramProgram
+    {
+        private static List<string> _anagramList = {};
+         
+
+    }
+}
