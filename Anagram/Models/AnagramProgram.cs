8f7c56dbad475bc4ba15bbff6fc944f728bfea0b
Ahmed Khokar <amkhokar@gmail.com>
2018-09-08 06:07:19 -0700

    add partial match method to AnagramProgram.cs

--- Anagram/Models/AnagramProgram.cs
+++ Anagram/Models/AnagramProgram.cs
@@ -18,11 +18,28 @@ namespace Anagram
                 {
                     _anagramList.Add(check); 
                 }
+                if (_inputWord.Length > check.Length)
+                {
+                    if (IsPartialMatch(_inputWord, check))
+                    {
+                        _anagramList.Add(check); 
+                    }
+                }
             }
         }
         public List<string> GetAnagrams()
         {
             return _anagramList; 
+        }
+
+        public void SetMatchWord(string matchWord)
+        {
+            _inputWord = matchWord; 
+        }
+
+        public string GetMatchWord()
+        {
+            return _inputWord;
         }
         public void AddToInputList(string inputWord)
         {
@@ -39,6 +56,19 @@ namespace Anagram
             string sortedWord = new string(charArray);  
             return sortedWord; 
         } 
+        public bool IsPartialMatch(string bigWord, string littleWord)
+        {
+            string[] littleArray = littleWord.Split(); 
+            bool IsMatch = true; 
+            foreach(string letterword in littleArray)
+            {
+                if (!bigWord.Contains(letterword))
+                {
+                    return false; 
+                }
+            }
+            return IsMatch; 
+        }
     }
     public class Program
     {
@@ -47,14 +77,25 @@ namespace Anagram
             AnagramProgram anagramz = new AnagramProgram(); 
             Console.WriteLine("Enter your word to match from: ");
             string input = Console.ReadLine(); 
-            anagramz.AddToInputList(input);
+            anagramz.SetMatchWord(input);
             while (input != "match")
             {
                 Console.WriteLine("Enter one word a time and type 'match' to check matches against original word");
-                input = Console.ReadLine();          
-            }         
+                input = Console.ReadLine();
+                anagramz.AddToInputList(input);          
+            }
+            Console.WriteLine("\n\nThese are the matches for '" + anagramz.GetMatchWord() + "'\n");
+            anagramz.SetAnagrams(); 
+            List<string> newList = new List<string>(); 
+            newList = anagramz.GetAnagrams();  
+            foreach(string word in newList)
+            {
+                Console.Write(word + "\n\n");
+            }
+            Console.WriteLine("Thank you for playing! Press CTRL + C To Quit!!!"); 
+            Initialize(); 
         }
-        public static void main() 
+        public static void Main() 
         { 
             Console.WriteLine("Welcome to Anagram Match!");  
             Initialize(); 
