2811587500eae688cd0014c3b80b28352f93199e
Ahmed Khokar <amkhokar@gmail.com>
2018-09-08 04:53:31 -0700

    add encapsulated variables

--- Anagram/Models/AnagramProgram.cs
+++ Anagram/Models/AnagramProgram.cs
@@ -6,24 +6,64 @@ namespace Anagram
     public class AnagramProgram
     {
         private static List<string> _anagramList = new List<string>();
-        
-        public void AddAnagram(string word) 
-        {
-            _anagramList.Add(word); 
-        }
+        private static string _inputWord = ""; 
+        private static List<string> _inputList = new List<string>(); 
 
+        public AnagramProgram(string word, List<string> checkList)
+        {
+            _inputWord = word; 
+            foreach(string check in checkList)
+            {
+                _inputList.Add(check); 
+            }
+
+        }
         public List<string> GetAnagrams()
         {
             return _anagramList; 
         }
 
-        public string ReturnSortedWord(string unsortedWord)
+        public List<string> GetInputList()
         {
-            char[] charArray = unsortedWord.ToCharArray();
+            return _inputList; 
+        }
+
+
+        public string ReturnSortedWord(string word)
+        {
+            char[] charArray = word.ToCharArray();
             Array.Sort(charArray);
             string sortedWord = new string(charArray);  
             return sortedWord; 
         }
+        public void AnagramMatcher()
+        {
+            foreach(string check in _inputList)
+            {
+                string sortedCheck = ReturnSortedWord(check);
+                string sortedWord = ReturnSortedWord(_inputWord);
+                if (sortedWord == sortedCheck)
+                {
+                    _anagramList.Add(check); 
+                }
+            }
+        } 
 
+        public static void Initialize()
+        {
+            List<string> inputList = new List<string>(); 
+            string word = ""; 
+            Console.WriteLine("");
+        }
     }
+
+    public class Program
+    {
+        public static void main() 
+        {
+            Console.WriteLine("Welcome to Anagram Match!"); 
+            AnagramProgram.Initialize(); 
+        }
+    }
+
 }
